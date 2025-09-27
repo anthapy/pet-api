3e4ef2a5009372219d892dcfd5acbe5218123ed1
anthany <itzmedevs@gmail.com>
2025-09-27 17:39:44 -0500

    Update pets.js

--- api/pets.js
+++ api/pets.js
@@ -1,1 +1,24 @@
 
+// api/pets.js
+export default function handler(req, res) {
+  const petList = [
+    { name: "Giant Chocolate Chicken", type: "Shiny", url: "https://api.bgsipets.com/s-gcc" },
+    { name: "Giant Chocolate Chicken", type: "Mythic", url: "https://api.bgsipets.com/m-gcc" },
+    { name: "Giant Chocolate Chicken", type: "Shiny Mythic", url: "https://api.bgsipets.com/sm-gcc" },
+    { name: "Leaf Doggy", type: "Shiny", url: "https://api.bgsipets.com/s-ld" },
+    { name: "Leaf Doggy", type: "Mythic", url: "https://api.bgsipets.com/m-ld" },
+    { name: "Leaf Doggy", type: "Shiny Mythic", url: "https://api.bgsipets.com/sm-ld" },
+    { name: "Leaf Kitty", type: "Shiny", url: "https://api.bgsipets.com/s-lk" },
+    { name: "Leaf Kitty", type: "Mythic", url: "https://api.bgsipets.com/m-lk" },
+    { name: "Leaf Kitty", type: "Shiny Mythic", url: "https://api.bgsipets.com/sm-lk" }
+  ];
+
+  // Convert array into nested JSON
+  const petsJSON = {};
+  petList.forEach(pet => {
+    if (!petsJSON[pet.name]) petsJSON[pet.name] = {};
+    petsJSON[pet.name][pet.type] = pet.url;
+  });
+
+  res.status(200).json(petsJSON);
+}
